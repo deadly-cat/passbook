802636190d63b1169f2bc67eaf4b49e59a0567df
ingvar.main@gmail.com <ingvar.main@gmail.com@c1fa565f-c334-a888-1135-041f2824896c>
2012-09-11 15:31:28 +0000

    * add link to bug tracker in error window * refactoring

--- src/home/ingvar/passbook/Passbook.java
+++ src/home/ingvar/passbook/Passbook.java
@@ -2,6 +2,7 @@ package home.ingvar.passbook;
 
 import home.ingvar.passbook.console.Console;
 import home.ingvar.passbook.ui.MainFrame;
+import home.ingvar.passbook.ui.views.ErrorDialog;
 import home.ingvar.passbook.utils.LOG;
 
 import javax.swing.JFrame;
@@ -23,6 +24,7 @@ public class Passbook {
 				JFrame frame = new MainFrame();
 				LOG.setFrame(frame);
 				frame.setVisible(true);
+				ErrorDialog.show(frame, "ERROR", "Message Message Message Message Message \n Message Message Message Message \nMessage Message Message Message Message Message Message", new RuntimeException("Runtime test exception"));
 			}
 		} catch(Exception e) {
 			LOG.error("Initialization error", "Something wrong see log for details", e); //TODO: i18n
